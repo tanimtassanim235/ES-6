18bb1cdbe244ef179e342fed85f565b38bf94197
Tanim Tassanim <tanimtassanim235@gmail.com>
2026-02-02 09:33:28 +0600

    let-const modified, access

--- let-const.js
+++ let-const.js
@@ -1,1 +1,14 @@
-const name = 'Bangladesh';
+const nameBD = 'Bangladesh';
+console.log(nameBD);
+
+let price  = 120;
+price = 200;
+console.log(price);
+
+const dress = ['shirt', 'pant', 'jacket'];
+dress.push('maflar');
+console.log(dress);
+
+const student = {name: 'rahim', marks: 50}
+student.name = 'jaman';
+console.log(student);
