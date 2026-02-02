bfbc3d6abeaba8375a225ff057fe617e13336ec6
Tanim Tassanim <tanimtassanim235@gmail.com>
2026-02-02 21:45:45 +0600

    template -string revise

---
+++ template-string.js
@@ -0,0 +1,12 @@
+const poem = `amm pata jora jora 
+marbo chabuk chorbo ghora`;
+console.log(poem);
+
+function sum(num1, num2)
+{
+    const result = num1 + num2;
+    const output = `Sum of ${num1} + ${num2} = ${result}`;
+    console.log(output);
+}
+
+sum(12, 14);
